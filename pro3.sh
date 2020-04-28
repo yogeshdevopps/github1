0924112ca88b471de230269242a34cba232f5beb
yogesh <yogesh13l.yk@gmail.com>
2020-04-28 20:10:17 +0530

    new scripts

---
+++ pro3.sh
@@ -0,0 +1,5 @@
+# write programread name and dispaly
+#!/bin/sh
+echo "what is your name ?"
+read name 
+echo " hello $name"
---
+++ pro4.sh
@@ -0,0 +1,8 @@
+#write program to read 2 numbers and dispaly sum,differnce product and division
+#!/bin/sh
+echo "enter 2 numbers"
+read m n
+echo $[m+n]
+echo $[m-n]
+echo $[m*n]
+echo $[m/n]
